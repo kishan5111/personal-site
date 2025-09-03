f454bd45a31b84a5fa5ccab6127da469d9963b45
kishan5111 <kishanvavdara@gmail.com>
2025-09-03 23:13:59 +0530

    Change intro on homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -41,14 +41,14 @@ const Index = () => {
           className="w-full space-y-4 md:space-y-6"
         >
         <p className="text-base md:text-lg text-gray-700 dark:text-gray-50">
-          <span className="font-bold text-gray-900 dark:text-white">Hey there!</span> I'm Kishan. This is my little corner on the Internet — make yourself at home! I'm currently working as a freelance data/ML engineer and building <a href="https://resume-crafter.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline hover:text-blue-800">ResumeCrafter</a> . I specialize in finetuning, optimizing inference, and deploying LLMs. 
+          <span className="font-bold text-gray-900 dark:text-white">Hey there!</span> I'm Kishan. This is my little corner on the Internet — make yourself at home! I'm currently working as a freelance data/ML engineer. I specialize in finetuning, optimizing inference, and deploying LLMs. 
           I transitioned into ML after completing my undergraduate studies in biotechnology (where I was a gold medalist!), 
           through self-study and online courses.
         </p>
           
           <p className="text-base md:text-lg text-gray-700 dark:text-gray-50">
             I'm also an active participant in Kaggle competitions, sharing datasets, models, and notebooks. 
-            I've published gold-medal-winning notebooks on fine-tuning and deploying LLaMA models using TPUs and GPUs, 
+            I've published gold-medal-winning notebooks on fine-tuning and deploying LLaMA, Gemma, and Qwen models using TPUs and GPUs, 
             as well as on training neural networks from scratch. Through my contributions, I recently became a 4x Kaggle Expert.
           </p>
         </motion.div>
